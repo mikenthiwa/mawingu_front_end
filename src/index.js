490c2a5809754713c50ca564e95b5f0ca285d773
Michael Nthiwa <mike.nthiwa@gmail.com>
2019-09-26 16:40:44 +0300

    Revert "Set up"

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux'
-import Routes from './router'
-import store from './redux/store'
-import * as serviceWorker from './serviceWorker';
 import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <Provider store={store}>
-    <Routes />
-  </Provider>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
